12f89633486ea71b2407fdc0781c34aee175a62c
João Amadeu <jmamadeu2000@gmail.com>
2022-01-06 05:56:41 +0100

    chore: theme and main

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import {
 } from "@expo-google-fonts/inter";
 import { Router } from "@routes/index";
 import AppLoading from "expo-app-loading";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -19,6 +20,7 @@ export default function App() {
   return (
     <>
       <Router />
+      <StatusBar style="light" />
     </>
   );
 }
